76c9cf2d345521109bf9b64f5ca7cfadff43b189
novosad <nnovosad@darwinapps.net>
2018-04-09 00:01:34 +0300

    Favicon

--- wp-content/themes/denant/header.php
+++ wp-content/themes/denant/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Денант</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico" />
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon.ico" />
     <?php wp_head(); ?>
 </head>
 <body>
